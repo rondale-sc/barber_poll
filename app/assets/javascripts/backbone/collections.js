0699c5bb20bad765ea06028b1d25f2f08661776a
Jonathan Jackson <jonathan.jackson1@me.com>
2014-03-17 00:53:08 -0400

    Explicitly sort AnswerCollection by id

--- app/assets/javascripts/backbone/collections.js
+++ app/assets/javascripts/backbone/collections.js
@@ -3,6 +3,9 @@
 
   barberPoll.Collections.AnswerCollection = Backbone.Collection.extend({
     model: barberPoll.Models.Answer,
+    comparator: function(collection) {
+      return( collection.get('id') );
+    },
     clearSelected: function(model){
       $.each(this.models, function(idx, m) {
         if(model.id !== m.id) { m.set('selected', false) };
