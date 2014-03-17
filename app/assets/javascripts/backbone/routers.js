cb194e66ef221a6bb3206032d5ce916cc7e843ec
Jonathan Jackson <jonathan.jackson1@me.com>
2014-03-17 00:18:53 -0400

    Undelege events from the main $el
    * This was to prevent event handlers on a.submit in both surveyInit view
    * and VoteView from colliding

--- app/assets/javascripts/backbone/routers.js
+++ app/assets/javascripts/backbone/routers.js
@@ -14,18 +14,21 @@
       ':id/r': 'resultInit'
     },
     voteInit: function(id){
+      this.el().undelegate();
       new barberPoll.Views.VoteContainerView({
         id: id,
         el: this.el()
       });
     },
     resultInit: function(id){
+      this.el().undelegate();
       new barberPoll.Views.ResultContainerView({
         id: id,
         el: this.el()
       });
     },
     surveyInit: function(){
+      this.el().undelegate();
       var app = new barberPoll.Views.SurveyContainerView({el: this.el()});
       return app.render();
     }
